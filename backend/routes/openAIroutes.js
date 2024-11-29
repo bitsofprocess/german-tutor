d46ddecb0e9725944ffe46d7b5504be1987e48b5
Dinelle Zipser <dinellezipser@gmail.com>
2024-11-29 08:04:37 +0100

    remove js files

--- backend/routes/openAIroutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const openAIcontrollers_1 = require("../controllers/openAIcontrollers");
-const router = (0, express_1.Router)();
-router.post('/verbs', openAIcontrollers_1.listVerbs);
-router.post('/extract', openAIcontrollers_1.extract);
-router.post('/concept', openAIcontrollers_1.conceptPractice);
-exports.default = router;
