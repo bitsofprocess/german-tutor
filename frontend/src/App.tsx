0f4fe7fc2a895b7b567c03104c64fa85fab2d0bc
Dinelle Zipser <dinellezipser@gmail.com>
2024-12-14 14:46:26 +0100

    remove commented code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,35 +5,6 @@ function App() {
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
-  // Specify the type for the event parameter
-  // const handleVerbRequest = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   setLoading(true);
-  //   setError('');
-
-  //   try {
-  //     const res = await fetch('http://localhost:4000/openai/verbs', {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify({ number }),
-  //     });
-
-  //     if (!res.ok) throw new Error('Network response was not ok');
-
-  //     const data = await res.json();
-  //     setVerbResponse(data.verbs);
-  //   } catch (err) {
-  //     // Type assertion for the error to access the message property
-  //     if (err instanceof Error) {
-  //       setError('Error: ' + err.message);
-  //     } else {
-  //       setError('An unknown error occurred');
-  //     }
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
   const handleRequest = async (concept: string) => {
     setLoading(true);
     setError('');
